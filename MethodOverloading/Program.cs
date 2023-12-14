6d30d2612a64a673f270b6e2ab8289fbc5329bec
Victor Salinas <victorsalinas6@gmail.com>
2023-12-14 14:03:43 -0600

    Finished the exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(2, 2));
+            Console.WriteLine(Add(3.2m, 2));
+            Console.WriteLine(Add(1, 0, true));
+
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool z)
+        {
+            return z ? x + y == 1 ? $"{x + y} dollar" 
+                                  : $"{x + y} dollars" 
+                                  : $"{x + y}";
         }
     }
 }
